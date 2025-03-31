eb4281f73a11695f4d1680d3655e155d6d9f814b
egorbelosohov <belosokhov.e@yandex.ru>
2025-03-31 07:41:37 +0300

    поправил

--- test/conftest.py
+++ test/conftest.py
@@ -1,23 +1,46 @@
-import pytest
+import pytest, os
 from selene import browser
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 from utils import attach
+from dotenv import load_dotenv
 
 
-@pytest.fixture(autouse=True)
-def setting_browser():
+@pytest.fixture(scope="session", autouse=True)
+def setup_env():
+    load_dotenv()
 
+
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_height = 1080
-    browser.config.window_width = 1080
-
     driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'  # вместо этой строки можно добавить другие опции
+    driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
-    yield
-    # browser.quit()
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "125.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    url = os.getenv('URL')
+
+    browser.config.driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@{url}/wd/hub",
+        options=options)
+
+    yield browser
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)

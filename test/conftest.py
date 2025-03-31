48c8858aa16e59506387fdbe14a234acecc4f179
egorbelosohov <belosokhov.e@yandex.ru>
2025-03-31 08:02:06 +0300

    поправил

--- test/conftest.py
+++ test/conftest.py
@@ -6,45 +6,36 @@ from utils import attach
 from dotenv import load_dotenv
 
 
-@pytest.fixture(scope="session", autouse=True)
-def setup_env():
-    load_dotenv()
-
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
-    browser.config.base_url = 'https://demoqa.com'
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
 
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "125.0",
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
 
+
     options.capabilities.update(selenoid_capabilities)
-
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
-    url = os.getenv('URL')
-
-    browser.config.driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@{url}/wd/hub",
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options)
 
-    yield browser
+    browser.config.driver = driver
+
+    yield
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
     attach.add_video(browser)
+
 
     browser.quit()

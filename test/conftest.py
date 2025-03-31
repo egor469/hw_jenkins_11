b574f06a6925f7126f7686f3ebad1b01800fe59b
egorbelosohov <belosokhov.e@yandex.ru>
2025-03-31 08:56:14 +0300

    поправил

--- test/conftest.py
+++ test/conftest.py
@@ -15,7 +15,7 @@ def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": "127.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True

2dfde919913ee8f1ff8ee2e6a0055f6068f99bf3
egorbelosohov <belosokhov.e@yandex.ru>
2025-04-07 18:00:48 +0300

    fix conftest

--- conftest.py
+++ conftest.py
@@ -6,22 +6,32 @@ from utils import attach
 from dotenv import load_dotenv
 import os
 
+DEFAULT_BROWSER_VERSION = "128.0"
 
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='128.0'
+    )
 @pytest.fixture(scope="session", autouse=True)
 def load_env():
     load_dotenv()
 
 
 @pytest.fixture(autouse=True)
-def setup_browser():
+def setup_browser(request):
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_height = 1080
     browser.config.window_width = 1920
 
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "128.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
